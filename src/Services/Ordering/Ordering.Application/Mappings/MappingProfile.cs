1519e16b6e2e351106551b0dd03bf490882e92d4
Vladimir Pantea <vpantea@gmail.com>
2021-05-10 10:43:51 +0300

    add Order, OrdersVm in AutoMapper profile

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 //using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 //using Ordering.Application.Features.Orders.Commands.UpdateOrder;
-//using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings
@@ -10,7 +10,7 @@ namespace Ordering.Application.Mappings
     {
         public MappingProfile()
         {
-            //CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, OrdersVm>().ReverseMap();
             //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
             //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
